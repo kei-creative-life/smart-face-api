e1c1f8b96d437bbe0b580e49289260734d47188e
kei_creativelife <68884990+kei-creative-life@users.noreply.github.com>
2023-04-30 18:24:31 +0900

    Add  Canvas Design Option

--- src/packages/TensorFlow.ts
+++ src/packages/TensorFlow.ts
@@ -32,57 +32,64 @@ const analyzeMetadata = async (metadata: string | Metadata) => {
   return fillMetadata(metadataJSON)
 }
 
-export const predictSSD = async () => {
-  await tf.ready()
-  const modelPath = 'https://tfhub.dev/tensorflow/tfjs-model/ssd_mobilenet_v2/1/default/1'
+interface DetectionStyle {
+  fontSize: string
+  fontFamily: string
+  labelStrokeColor: string
+  labelBackgroundColor: string
+}
 
-  const model = await tf.loadGraphModel(modelPath, { fromTFHub: true })
-  const mysteryImage = document.getElementById('mask') as HTMLImageElement
-  const myTensor = tf.browser.fromPixels(mysteryImage)
+const defaultDetectionStyle = {
+  fontSize: '12px',
+  fontFamily: 'sans-serif',
+  labelStrokeColor: '#0F0',
+  labelBackgroundColor: '#0F0',
+}
+
+export const predictObjects = async (targetId: string, detectionStyle: DetectionStyle = defaultDetectionStyle) => {
+  await tf.ready()
+
+  const modelURL = 'https://tfhub.dev/tensorflow/tfjs-model/ssd_mobilenet_v2/1/default/1'
+  const model = await loadGraphModelFromURL(modelURL)
+
+  const targetElement = document.getElementById(targetId) as HTMLVideoElement | HTMLCanvasElement | HTMLImageElement
+  const tensorImage = tf.browser.fromPixels(targetElement)
 
   // [1, height, width, 3]の形状に変更
-  const readyfied = tf.expandDims(myTensor, 0)
-
+  const readyfied = tf.expandDims(tensorImage, 0)
   const results: any = await model.executeAsync(readyfied)
 
-  // Prep Canvas
-  const detection = document.getElementById('detection') as HTMLCanvasElement
-  const ctx = detection.getContext('2d')
-  const imgWidth = mysteryImage.width
-  const imgHeight = mysteryImage.height
-  detection.width = imgWidth
-  detection.height = imgHeight
+  const detectionCanvas = document.getElementById('detection') as HTMLCanvasElement
+  const ctx = detectionCanvas.getContext('2d')
+  const imgWidth = targetElement.width
+  const imgHeight = targetElement.height
+
+  detectionCanvas.width = imgWidth
+  detectionCanvas.height = imgHeight
+
+  const { fontSize, fontFamily, labelStrokeColor, labelBackgroundColor } = detectionStyle
+
   if (ctx) {
-    ctx.font = '16px sans-serif'
+    ctx.font = fontSize + ' ' + fontFamily
     ctx.textBaseline = 'top'
   }
 
-  // Get a clean tensor of indices
   const detectionThreshold = 0.3
   const iouThreshold = 0.5
   const maxBoxes = 20
+
   // results[0]: 最大1の確率値を90個持つ1,917個の検出結果
   const prominentDetection = tf.topk(results[0])
-  prominentDetection.indices.print()
-  prominentDetection.values.print()
+
   const justBoxes = results[1].squeeze()
   const justValues = prominentDetection.values.squeeze()
 
-  // Move results back to JavaScript in parallel
   const [maxIndices, scores, boxes] = await Promise.all([prominentDetection.indices.data(), justValues.array(), justBoxes.array()])
 
-  // https://arxiv.org/pdf/1704.04503.pdf, use Async to keep visuals
-  const nmsDetections = await tf.image.nonMaxSuppressionWithScoreAsync(
-    justBoxes, // [numBoxes, 4]
-    justValues, // [numBoxes]
-    maxBoxes,
-    iouThreshold,
-    detectionThreshold,
-    1 // 0 is normal NMS, 1 is Soft-NMS for overlapping support
-  )
+  const nmsDetections = await tf.image.nonMaxSuppressionWithScoreAsync(justBoxes, justValues, maxBoxes, iouThreshold, detectionThreshold, 1)
 
   const chosen = await nmsDetections.selectedIndices.data()
-  // Mega Clean
+
   tf.dispose([
     results[0],
     results[1],
@@ -91,41 +98,51 @@ export const predictSSD = async () => {
     nmsDetections.selectedScores,
     prominentDetection.indices,
     prominentDetection.values,
-    myTensor,
+    tensorImage,
     readyfied,
     justBoxes,
     justValues,
   ])
 
+  const classes = [] as any
+
   chosen.forEach((detection) => {
     if (ctx) {
-      ctx.strokeStyle = '#0F0'
+      ctx.strokeStyle = labelStrokeColor
       ctx.lineWidth = 4
       ctx.globalCompositeOperation = 'destination-over'
+
       const detectedIndex = maxIndices[detection]
       const detectedClass = CLASSES[detectedIndex]
       const detectedScore = scores[detection]
       const dBox = boxes[detection]
 
-      // No negative values for start positions
+      classes.push({
+        className: detectedClass,
+        probability: detectedScore,
+      })
+
       const startY = dBox[0] > 0 ? dBox[0] * imgHeight : 0
       const startX = dBox[1] > 0 ? dBox[1] * imgWidth : 0
       const height = (dBox[2] - dBox[0]) * imgHeight
       const width = (dBox[3] - dBox[1]) * imgWidth
       ctx.strokeRect(startX, startY, width, height)
-      // Draw the label background.
+
       ctx.globalCompositeOperation = 'source-over'
-      ctx.fillStyle = '#0B0'
+      ctx.fillStyle = labelBackgroundColor
+
       const textHeight = 16
       const textPad = 4
       const label = `${detectedClass} ${Math.round(detectedScore * 100)}%`
       const textWidth = ctx.measureText(label).width
+
       ctx.fillRect(startX, startY, textWidth + textPad, textHeight + textPad)
-      // Draw the text last to ensure it's on top.
       ctx.fillStyle = '#000000'
       ctx.fillText(label, startX, startY)
     }
   })
+
+  return classes
 }
 
 export class TensorFlow {
@@ -140,8 +157,6 @@ export class TensorFlow {
   }
 
   async predict(image: HTMLVideoElement | HTMLCanvasElement | HTMLImageElement) {
-    // await this.predictSSD()
-
     const logits = tf.tidy(() => {
       const croppedImage = convertCanvasDataToTensor(image)
       return this.model.predict(croppedImage)
@@ -150,8 +165,6 @@ export class TensorFlow {
     const values = await (logits as tf.Tensor<tf.Rank>).data()
 
     const classes = []
-
-    console.log(values)
 
     for (let i = 0; i < values.length; i++) {
       classes.push({
@@ -164,6 +177,11 @@ export class TensorFlow {
 
     return classes
   }
+}
+
+export async function loadGraphModelFromURL(modelURL: string) {
+  const customModel = await tf.loadGraphModel(modelURL, { fromTFHub: true })
+  return customModel
 }
 
 export async function loadFromURL(modelURL: string, metadata?: string | Metadata) {
