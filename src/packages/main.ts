f82c1f58c0ee9965b1b794ba5104e205ca0f2e4c
kei_creativelife <68884990+kei-creative-life@users.noreply.github.com>
2023-05-01 23:32:31 +0900

    Hide unnecessary Code

--- src/packages/main.ts
+++ src/packages/main.ts
@@ -4,14 +4,14 @@ export { TensorFlowModule }
 
 // SSD Test
 // const getImage = document.getElementById('mask') as HTMLImageElement
-const defaultDetectionStyle = {
-  fontSize: '16px',
-  fontFamily: 'sans-serif',
-  labelStrokeColor: 'red',
-  labelBackgroundColor: 'red',
-}
-const test = await TensorFlowModule.predictObjects('sample', defaultDetectionStyle)
-console.log(test)
+// const defaultDetectionStyle = {
+//   fontSize: '16px',
+//   fontFamily: 'sans-serif',
+//   labelStrokeColor: 'red',
+//   labelBackgroundColor: 'red',
+// }
+// const test = await TensorFlowModule.predictObjects('sample')
+// console.log(test)
 
 // Detect Model Test
 // const URL = 'https://teachablemachine.withgoogle.com/models/WdEY9SIhG/'
